bbcbe050ed25aed03e1054346a95e8c1f865ea14
kiarash aghakasiri <kiarash@kiarashs-NewMacBook-Pro.local>
2024-07-11 22:18:52 -0600

    update

--- js/image_caption.js
+++ js/image_caption.js
@@ -8,12 +8,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
             event.preventDefault();
             modal.style.display = "block";
             modalImg.src = item.href;
-            captionText.innerHTML = item.querySelector('img').alt;
+            
+            // Get data attributes from the image
+            var img = item.querySelector('img');
+            var imgName = img.getAttribute('data-name');
+            var imgSize = img.getAttribute('data-size');
+            var imgMedium = img.getAttribute('data-medium');
+            var imgPrice = img.getAttribute('data-price');
+
+            // Set the caption content with different fonts
+            captionText.innerHTML = `
+                <span class='img-name'>${imgName}</span><br>
+                <span class='img-size'>${imgSize}</span><br>
+                <span class='img-medium'>${imgMedium}</span><br>
+                <span class='img-price'>${imgPrice}</span>
+            `;
         });
     });
 
-    var span = document.getElementsByClassName("close")[0];
-    span.onclick = function() { 
-        modal.style.display = "none";
-    }
-});
+            var span = document.getElementsByClassName("close")[0];
+            span.onclick = function() { 
+                modal.style.display = "none";
+            }
+        });
